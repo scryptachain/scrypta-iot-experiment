24470bd24c2e56190f6eb69ccfc64e2710b912d5
turinglabsorg <turinglabs@criptext.com>
2020-03-28 09:31:16 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scrypta IoT demo application
+
+Use this application to create an history temperature graph using the blockchain.
+
+This script fetches and saves the informations every 1 minute, so you need 1.44 LYRA each day.
+
+Use simple with:
+```node index.js```
+
+Or if you want to create a persistent script using PM2:
+```pm2 start index.js```
---
+++ index.js
@@ -0,0 +1,57 @@
+const ScryptaCore = require('@scrypta/core')
+require('dotenv').config()
+const { exec } = require("child_process");
+
+const privkey = process.env.PRIVKEY
+const scrypta = new ScryptaCore
+const si = require('systeminformation')
+
+if (privkey !== undefined) {
+
+    function storeTemperature() {
+        exec("/opt/vc/bin/vcgencmd measure_temp", async (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            let response = stdout.trim().replace('temp=', '').replace("'", '')
+            let importedkey = await scrypta.importPrivateKey(privkey, privkey)
+            let address = importedkey.pub
+            let balance = await scrypta.get('/balance/' + address)
+            if (balance.balance > 0.001) {
+
+                let sysinfo = await si.system()
+                delete sysinfo.uuid
+                delete sysinfo.sku
+                sysinfo.temp = response
+                let dataToWrite = JSON.stringify(sysinfo)
+                let write = await scrypta.post('/write',
+                    {
+                        dapp_address: address,
+                        private_key: privkey,
+                        data: dataToWrite,
+                        collection: 'RaspTemperature'
+                    })
+                console.log('DATA WRITED')
+
+            } else {
+                console.log('INSUFFICIENT FUNDS')
+            }
+        });
+    }
+    
+    storeTemperature()
+    setInterval(function () {
+        storeTemperature()
+    }, 60000)
+
+} else {
+
+    console.log('NO PRIVATE KEY FOUND, PLEASE WRITE IT INTO .env FILE')
+
+}
+
